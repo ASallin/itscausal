1f8f7202d20871f8b90f49ff5ab2d206b8df5d8e
Aurelien Sallin <aurelien.sallin@swica.ch>
2023-08-23 18:45:59 +0200

    add example and test forecastITS

--- R/forecastITS.R
+++ R/forecastITS.R
@@ -29,6 +29,16 @@
 #' @export
 #'
 #' @examples
+#' 
+#' time <- rep(c(1:50), 5)
+#' id   <- rep(1:5, each = 50)
+#' year <- rep(c(rep(1990, 12), rep(1991, 12), rep(1992, 12), rep(1993, 12), rep(1994, 2)), 5)
+#' y    <- time + rnorm(50, 0, 1) + (time>40)*rnorm(10, 2, 2)
+#' df   <- data.frame(id, time, year, y)
+#' f <- forecastITS(data = df, time = "time", INDEX = 40, covariates_time = c("year"), key = "id", 
+#'                 y = "y")
+#' 
+
 
 
 forecastITS <- function(data, time, INDEX = 0L, WINDOW = 12L, STEPS = as.integer(WINDOW/3),
@@ -60,7 +70,7 @@ forecastITS <- function(data, time, INDEX = 0L, WINDOW = 12L, STEPS = as.integer
 
 
   # Prepare folds for cross-validation
-  data[, "cv"] <- crossValidateITS(data, id = "id", k = K)
+  data[, "cv"] <- crossValidateITS(data, id = key, k = K)
 
   forecast <- list()
 
